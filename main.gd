09364208300bb047d59ee0e990fc2b35f89f562b
barre <barrettab456@gmail.com>
2026-02-20 22:55:45 -0500

    coins

--- main.gd
+++ main.gd
@@ -2,7 +2,7 @@ extends Node
 
 var customers = 0
 var table = 0
-var coins = 0 
+var coins = 50
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$head_count.text = "Head count:"
@@ -11,20 +11,31 @@ func _ready() -> void:
 	$table_count.text = "Table count:"
 	$table_count.modulate = Color.AQUAMARINE
 	
-	$coin_counter.start()
+	$coin_count.text = "Coin Count:" + str(coins)
+	$coin_count.modulate = Color.AQUAMARINE
+	
 	
 func _input(event):
 	if event.is_action_pressed("add_person"):
 		customers += 1
-		coins = $coin_counter 
-		$coin_count = "Coin count:" + str(coins)
+		coins += 50
+		$coin_count.text = "Coin Count:" + str(coins)
+		if coins >= 100:
+			$coin_count.modulate = Color.AQUAMARINE
+		
 		if customers > table*4:
 			$head_count.modulate = Color.RED
 		$head_count.text = "Head count:" + str(customers)
 		
 	if event.is_action_pressed("add_table"):
-		table += 1
-		$table_count.text = "Table count:" + str(table)
-		if customers < table*4:
-			$head_count.modulate = Color.AQUAMARINE
+		if coins >= 100:
+			table += 1
+			$table_count.text = "Table count:" + str(table)
+			coins -= 100
+			$coin_count.text = "Coin Count:" + str(coins)
+			if customers < table*4:
+				$head_count.modulate = Color.AQUAMARINE
+		else:
+			$coin_count.modulate = Color.RED
+			
 		
