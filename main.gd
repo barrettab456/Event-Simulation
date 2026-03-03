62b9a1ea1e954c549c8564072aa3a9a5810b4697
barre <barrettab456@gmail.com>
2026-03-02 22:50:55 -0500

    organize main

--- main.gd
+++ main.gd
@@ -1,34 +1,51 @@
 extends Node
 
-var coins = 90
+var coins = 0
+var guests = 0
+var tables = 0 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$coin_count.text = "Coin Count:" + str(coins)
 	$coin_count.modulate = Color.RED
 	
-	$add_guest.text = "Guest amount:"
-	$add_guest.modulate = Color.AQUAMARINE
+	$head_count.text = "Head amount:"
+	$head_count.modulate = Color.AQUAMARINE
 	
 	$table_count.text = "Table count:"
 	$table_count.modulate = Color.AQUAMARINE
 	
 func _input(event):
 	if event.is_action_pressed("add_person"):
-		$guests.new_guest()
+		new_guest()
 	if event.is_action_pressed("add_table"):
-		$tables.new_table()
-	sufficent_funds()
+		new_table()
+	
+func new_guest():
+	guests += 1
+	coins += 50
+	update_hud()
+	
+func new_table():
+	if coins >= 100:
+			tables += 1
+			coins -= 100
+	update_hud()
+			
+func update_hud():
+	$coin_count.text = "Coin Count:" + str(coins)
+	if coins < 100: 
+		$coin_count.modulate = Color.RED
+	else: 
+		$coin_count.modulate = Color.AQUAMARINE
+	$head_count.text = "Head count:" + str(guests)
+	$table_count.text = "Table count:" + str(tables)
+	if tables < guests*4: 
+		$table_count.modulate = Color.RED
+	else: 
+		$table_count.modulate = Color.AQUAMARINE
 		
 func _on_add_person_pressed() -> void:
-	$guests.new_guest()
-	sufficent_funds()
+	new_guest()
 	
 func _on_add_table_pressed() -> void:
-	$tables.new_table()
-	sufficent_funds()
-	
-func sufficent_funds():	
-	if $main.coins >= 100:
-		$coin_count.modulate = Color.AQUAMARINE
-	if $guests.guests > $tables.table*4:
-		$guests/head_count.moducalte = Color.RED
+	new_table()
